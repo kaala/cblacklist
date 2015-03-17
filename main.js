38a805bdbd3307b5c897dcfab73a973502b6f843
kaala <kaala@macbook>
2015-03-17 21:56:26 +0800

    code formatter

--- main.js
+++ main.js
@@ -1,60 +1,62 @@
-function isUrlInsideList(url,blacklist){
-    for(var idx=0; idx<blacklist.length; idx++){
-        var enc=blacklist[idx];
-        var regex=new RegExp(enc,'i');
-        if(regex.test(url)){
+function isUrlInsideList(url, blacklist) {
+    for (var idx = 0; idx < blacklist.length; idx++) {
+        var enc = blacklist[idx];
+        var regex = new RegExp(enc, 'i');
+        if (regex.test(url)) {
             return true;
         }
     }
     return false;
 }
 
-function convertListToRegExp(text){
-    var lines=[];
-    if(text){
-        lines=text.split(/\n/gm);
+function convertListToRegExp(text) {
+    var lines = [];
+    if (text) {
+        lines = text.split(/\n/gm);
     }
-    var blacklist=[];
-    for(var idx=0; idx<lines.length; idx++){
-        var regex=lines[idx];
-        if(regex.length){
-            regex=regex.replace(/\//g,'\\/');//=/->\/
-            regex=regex.replace(/\./g,'\\.');//=.->\.
-            regex=regex.replace(/\*\*/g,'.+');//=**->.+
-            regex=regex.replace(/\*/g,'[^/]+');//=*->[^/]+
+    var blacklist = [];
+    for (var idx = 0; idx < lines.length; idx++) {
+        var regex = lines[idx];
+        if (regex.length) {
+            regex = regex.replace(/\//g, '\\/'); //=/->\/
+            regex = regex.replace(/\./g, '\\.'); //=.->\.
+            regex = regex.replace(/\*\*/g, '.+'); //=**->.+
+            regex = regex.replace(/\*/g, '[^/]+'); //=*->[^/]+
             blacklist.push(regex);
         }
     }
     return blacklist;
 }
 
-var opts={};
-opts.blacklist=[];
+var opts = {};
+opts.blacklist = [];
 
 chrome.storage.sync.get(
     null,
-    function(items){
-        var text=items.text;
-        opts.blacklist=convertListToRegExp(text);
+    function(items) {
+        var text = items.text;
+        opts.blacklist = convertListToRegExp(text);
     }
 );
 
 chrome.webRequest.onBeforeRequest.addListener(
     function(info) {
         var url = info.url;
-        var blacklist=opts.blacklist;
-        if(isUrlInsideList(url,blacklist)){
-            return {cancel: true};
-        }else{
-            return {cancel: false};
+        var blacklist = opts.blacklist;
+        if (isUrlInsideList(url, blacklist)) {
+            return {
+                cancel: true
+            };
+        } else {
+            return {
+                cancel: false
+            };
         }
-    },
-    {
-        urls: [            
+    }, {
+        urls: [
             "<all_urls>"
         ]
-    },
-    [
+    }, [
         "blocking"
     ]
 );
--- options.js
+++ options.js
@@ -1,47 +1,47 @@
-var storage=chrome.storage.sync;
-var runtime=chrome.runtime;
+var storage = chrome.storage.sync;
+var runtime = chrome.runtime;
 
-var el=function(obj){
+var el = function(obj) {
     return document.getElementById(obj);
 };
 
-function save(){
-    var key=this.id;
-    var value=this.value;
-    if(value.length){
-        var opts={};
-        opts[key]=value;
+function save() {
+    var key = this.id;
+    var value = this.value;
+    if (value.length) {
+        var opts = {};
+        opts[key] = value;
         storage.set(opts);
-    }else{
+    } else {
         storage.remove(key);
     }
 }
 
 function restore() {
-    storage.get(null,function(items){
+    storage.get(null, function(items) {
         console.log(items);
-        var keys=Object.keys(items);
-        for (idx in keys){
-            var key=keys[idx];
-            var value=items[key];
-            var dom=el(key);
-            if(dom){
-                dom.value=value;
+        var keys = Object.keys(items);
+        for (idx in keys) {
+            var key = keys[idx];
+            var value = items[key];
+            var dom = el(key);
+            if (dom) {
+                dom.value = value;
             }
         }
     });
 }
 
-document.addEventListener('DOMContentLoaded',restore);
-el('text').addEventListener('blur',save);
+document.addEventListener('DOMContentLoaded', restore);
+el('text').addEventListener('blur', save);
 
-function reload(){
+function reload() {
     runtime.reload();
 }
 
-function reset(){
+function reset() {
     storage.clear();
-    window.location=window.location;
+    window.location = window.location;
 }
 
-el('reload').addEventListener('click',reload);
+el('reload').addEventListener('click', reload);
